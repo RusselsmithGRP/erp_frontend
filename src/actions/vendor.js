24162cce8e3965e15a1dfea5529fa4c0b7399987
Idowu <iarifayan@russelsmith.local>
2019-08-26 12:11:10 +0100

    Updated vendors.js actions

--- src/actions/vendor.js
+++ src/actions/vendor.js
@@ -1,56 +1,69 @@
-import { FETCH_VENDOR, FETCH_VENDORS, ADD_VENDOR, UPDATE_VENDOR, APPROVE_VENDOR, DELETE_VENDOR, RECIEVE_GENERAL_INFO_DATA, RECIEVE_BUSINESS_INFO_DATA, RECIEVE_WORK_REFERENCE_DATA, RECIEVE_BANK_DETAIL_DATA } from './index';
-import * as loadAction from './loading';
+import {
+  FETCH_VENDOR,
+  FETCH_VENDORS,
+  ADD_VENDOR,
+  UPDATE_VENDOR,
+  APPROVE_VENDOR,
+  DELETE_VENDOR,
+  RECIEVE_GENERAL_INFO_DATA,
+  RECIEVE_BUSINESS_INFO_DATA,
+  RECIEVE_WORK_REFERENCE_DATA,
+  RECIEVE_BANK_DETAIL_DATA
+} from "./index";
+import * as loadAction from "./loading";
 import MiddleWare from "../middleware/api";
 
-export function fetchVendorAction(arry){
-    return {type: FETCH_VENDOR, data:arry};
+export function fetchVendorAction(arry) {
+  return { type: FETCH_VENDOR, data: arry };
 }
-export function fetchVendorsAction(arry){
-    return {type: FETCH_VENDORS, data:arry};  
+export function fetchVendorsAction(arry) {
+  return { type: FETCH_VENDORS, data: arry };
 }
-export function addVendorAction(arry){
-    return {type: ADD_VENDOR, data:arry};  
+export function addVendorAction(arry) {
+  return { type: ADD_VENDOR, data: arry };
 }
-export function updateVendorsAction(arry){
-    return {type: UPDATE_VENDOR, data:arry};  
+export function updateVendorsAction(arry) {
+  return { type: UPDATE_VENDOR, data: arry };
 }
-export function approveVendorsAction(arry){
-    return {type: APPROVE_VENDOR, data:arry};  
+export function approveVendorsAction(arry) {
+  return { type: APPROVE_VENDOR, data: arry };
 }
-export function deleteVendorsAction(arry){
-    return {type: DELETE_VENDOR, data:arry};  
+export function deleteVendorsAction(arry) {
+  return { type: DELETE_VENDOR, data: arry };
 }
-export function getGeneralInfoInputs(dispatch, d){
-dispatch({type: RECIEVE_GENERAL_INFO_DATA, data:d});
+export function getGeneralInfoInputs(dispatch, d) {
+  dispatch({ type: RECIEVE_GENERAL_INFO_DATA, data: d });
 }
-export function getBusinessInfoInputs(dispatch, d){
-    dispatch({type: RECIEVE_BUSINESS_INFO_DATA, data:d});
+export function getBusinessInfoInputs(dispatch, d) {
+  dispatch({ type: RECIEVE_BUSINESS_INFO_DATA, data: d });
 }
-export function getBankDetailInputs(dispatch, d){
-    dispatch({type: RECIEVE_BANK_DETAIL_DATA, data:d});
+export function getBankDetailInputs(dispatch, d) {
+  dispatch({ type: RECIEVE_BANK_DETAIL_DATA, data: d });
 }
-export function getWorkReferenceInputs(dispatch, d){
-    dispatch({type: RECIEVE_WORK_REFERENCE_DATA, data:d});
+export function getWorkReferenceInputs(dispatch, d) {
+  dispatch({ type: RECIEVE_WORK_REFERENCE_DATA, data: d });
 }
-export function findAllVendors(props, type=''){
-
-    let middleware = new MiddleWare(props.user.token);
-    let endpoint = '/vendors';
-    if(type === 'pending'){
-        endpoint = endpoint+'/pending';
-    }else if(type === 'approved'){
-        endpoint = endpoint+'/approved';
-    }else if(type === "unapproved"){
-        endpoint = endpoint+'/unapproved';
-    }else if(type === "blacklisted"){
-        endpoint = endpoint+'/blacklisted';
-    }else if(type === "new"){
-        endpoint += '/new';
-    }
-    props.dispatch(loadAction.Loading());
-        return middleware.makeConnection(endpoint ,'GET').then((response) => response.json()).then((responseJson)=>{
-            let datas =[];
-/*             responseJson.map((row)=>{
+export function findAllVendors(props, type = "") {
+  let middleware = new MiddleWare(props.user.token);
+  let endpoint = "/vendors";
+  if (type === "pending") {
+    endpoint = endpoint + "/pending";
+  } else if (type === "approved") {
+    endpoint = endpoint + "/approved";
+  } else if (type === "unapproved") {
+    endpoint = endpoint + "/unapproved";
+  } else if (type === "blacklisted") {
+    endpoint = endpoint + "/blacklisted";
+  } else if (type === "new") {
+    endpoint += "/new";
+  }
+  props.dispatch(loadAction.Loading());
+  return middleware
+    .makeConnection(endpoint, "GET")
+    .then(response => response.json())
+    .then(responseJson => {
+      let datas = [];
+      /*             responseJson.map((row)=>{
                 let arry = [];
                 arry.push(row._id, row.general_info.company_name, row.general_info.contact_name, row.general_info.contact_phone,
                 row.general_info.contact_email, row.status);
@@ -60,85 +73,94 @@ export function findAllVendors(props, type=''){
                 headerRow: ["Company Name", "Contact Person", "Contact Telephone", "Contact Email", "Actions"],
                 footerRow: ["Company Name", "Contact Person", "Contact Telephone", "Contact Email", "Actions"],
                 dataRows: datas} */
-            props.dispatch(fetchVendorsAction(responseJson));
-            props.dispatch(loadAction.LoadingSuccess());
-        });
+      props.dispatch(fetchVendorsAction(responseJson));
+      props.dispatch(loadAction.LoadingSuccess());
+    });
 }
 
-export function findVendorByUserId(props,userId){
-    if(typeof(userId) == "undefined")return;
-    let middleware = new MiddleWare(props.user.token);
-    props.dispatch(loadAction.Loading());
-    return middleware.makeConnection('/vendors/'+userId,'GET').then((response) => {
-        return response.json()
-    }).then(        
-        (responseJson)=>{
-            props.dispatch({type: 'UPDATE_VENDOR', data:responseJson[0]});
-            props.dispatch(loadAction.LoadingSuccess());
-        }
-    );
+export function findVendorByUserId(props, userId) {
+  if (typeof userId == "undefined") return;
+  let middleware = new MiddleWare(props.user.token);
+  props.dispatch(loadAction.Loading());
+  return middleware
+    .makeConnection("/vendors/" + userId, "GET")
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      props.dispatch({ type: "UPDATE_VENDOR", data: responseJson[0] });
+      props.dispatch(loadAction.LoadingSuccess());
+    });
 }
 
-export function findVendorById(props,vendorId){
-    let middleware = new MiddleWare(props.user.token);
-    props.dispatch(loadAction.Loading());
-    return middleware.makeConnection('/vendors/one/'+vendorId,'GET').then((response) => {
-        return response.json()
-    }).then(        
-        (responseJson)=>{
-            props.dispatch({type: 'UPDATE_VENDOR', data:responseJson[0]});
-            props.dispatch(loadAction.LoadingSuccess());
-        }
-    );
+export function findVendorById(props, vendorId) {
+  let middleware = new MiddleWare(props.user.token);
+  props.dispatch(loadAction.Loading());
+  return middleware
+    .makeConnection("/vendors/one/" + vendorId, "GET")
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      props.dispatch({ type: "UPDATE_VENDOR", data: responseJson[0] });
+      props.dispatch(loadAction.LoadingSuccess());
+    });
 }
 
-export function searchVendor(token,search, callback){
-    let m = new MiddleWare(token);
-    ///props.dispatch(loadAction.Loading());
-    return m.makeConnection('/vendors/search/'+search, m.GET).then((response) => {
-        return response.json()
-    }).then(        
-        (responseJson)=>{
-            callback(responseJson);
-        }
-    );
+export function searchVendor(token, search, callback) {
+  let m = new MiddleWare(token);
+  ///props.dispatch(loadAction.Loading());
+  return m
+    .makeConnection("/vendors/search/" + search, m.GET)
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      callback(responseJson);
+    });
 }
 
-export function submitVendorDetailsViaUserId(dispatch, userId, d){
-    let middleware = new MiddleWare();
-    let data = {};
-    data.payload = d;
-    data.key = "user";
-    data.value = userId;
-    dispatch(loadAction.Loading());
-    middleware.makeConnection('/vendors','PUT', data).then(
-      (result)=>{
-        if(result.ok && result.statusText == "OK" && result.status == 200 ) 
-            dispatch(loadAction.LoadingSuccess("Saved Successfully"));
-        }
-    ).then(()=>{
-        dispatch({type: 'UPDATE_VENDOR', data:d});
-    }).catch((e)=>{
+export function submitVendorDetailsViaUserId(dispatch, userId, d) {
+  let middleware = new MiddleWare();
+  let data = {};
+  data.payload = d;
+  data.key = "user";
+  data.value = userId;
+  dispatch(loadAction.Loading());
+  middleware
+    .makeConnection("/vendors", "PUT", data)
+    .then(result => {
+      if (result.ok && result.statusText == "OK" && result.status == 200)
+        dispatch(loadAction.LoadingSuccess("Saved Successfully"));
+    })
+    .then(() => {
+      dispatch({ type: "UPDATE_VENDOR", data: d });
+    })
+    .catch(e => {
       console.log(e);
-    })
+    });
 }
 
-export function deleteVendor(props, user){
-    let middleware = new MiddleWare(props.user.token);
-    middleware.makeConnection('/vendors/deletevendor','DELETE', {"user":user}).then((res)=>{
-      console.log(user)
-    }).catch((e)=>{
+export function deleteVendor(props, user) {
+  let middleware = new MiddleWare(props.user.token);
+  middleware
+    .makeConnection("/vendors/deletevendor", "DELETE", { user: user })
+    .then(res => {
+      console.log(user);
+    })
+    .catch(e => {
       console.log("the error" + e);
-    })
+    });
 }
 
-export function getVendorEvaluation(token, vendorId, callback){
-    let middleware = new MiddleWare(token);
-    return middleware.makeConnection('/vendorevaluation/view/'+vendorId,'GET').then((response) => {
-        return response.json()
-    }).then(        
-        (responseJson)=>{
-            callback(responseJson);
-        }
-    );
+export function getVendorEvaluation(token, vendorId, callback) {
+  let middleware = new MiddleWare(token);
+  return middleware
+    .makeConnection("/vendorevaluation/view/" + vendorId, "GET")
+    .then(response => {
+      return response.json();
+    })
+    .then(responseJson => {
+      callback(responseJson);
+    });
 }
