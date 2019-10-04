7312f24b45da2711ab80d270c9d61f1320c30e0a
Idowu <iarifayan@russelsmith.local>
2019-10-04 12:11:22 +0100

    Created a currency formatter in currencies.js

--- src/utility/Currencies.js
+++ src/utility/Currencies.js
@@ -1,23 +1,36 @@
 import React, { Component } from "react";
 const style = {
-    fontSize:"9px"
-}
+  fontSize: "9px"
+};
 
 export const CURRENCIES = [
-    { value: "0", label: "₦", code: "NGN"},
-    { value: "1", label: "$", code:"USD" },
-    { value: "2", label: "£", code: "GBP" },
-    { value: "3", label: "€", code: "EUR" }
-]
+  { value: "0", label: "₦", code: "NGN" },
+  { value: "1", label: "$", code: "USD" },
+  { value: "2", label: "£", code: "GBP" },
+  { value: "3", label: "€", code: "EUR" }
+];
 
-export function getCurrency(value){
-    if(!value) return;
-    const currency = CURRENCIES.filter((k)=>k.value==value);
-    return currency[0].label;
+export function getCurrency(value) {
+  if (!value) return;
+  const currency = CURRENCIES.filter(k => k.value == value);
+  return currency[0].label;
 }
 
-export function getCurrencyCode(value){
-    if(!value) return;
-    const currency = CURRENCIES.filter((k)=>k.value==value);
-    return <span style={style} dangerouslySetInnerHTML={{__html: currency[0].code}} /> 
+export function getCurrencyCode(value) {
+  if (!value) return;
+  const currency = CURRENCIES.filter(k => k.value == value);
+  return (
+    <span
+      style={style}
+      dangerouslySetInnerHTML={{ __html: currency[0].code }}
+    />
+  );
+}
+
+export function formatCurrency(locales, code, amount) {
+  // Example of locales: en-US, en-NG --> for Nigeria
+  return new Intl.NumberFormat(locales, {
+    style: "currency",
+    currency: code
+  }).format(amount);
 }
